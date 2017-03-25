ecc3c454bc1c19ebea0ff4a78c3eb96c41022ab7
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-03-24 22:07:10 -0700

    initial about section creation

---
+++ frontend/components/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class About extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <section className='about'>
+        <p1>
+          A & N is love, A & N is life.
+        </p1>
+      </section>
+    )
+  }
+}
+
+export default About;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import People from './about_people/people';
 import Splash from './splash/splash';
 import NavBar from './nav_bar/nav_bar';
+import About from './about/about';
 
 
 const Root = () => {
   return (
-    <section>
+    <section className='total-page'>
       <Splash/>
       <NavBar/>
+      <About/>
       <People/>
     </section>
   );
