0849c3546e900c6e82dab7a3be03bb6424fe20d9
thomaschesnutt <tom@antechserv.com>
2017-04-17 14:20:43 -0700

    TomC Headshot 2

--- frontend/components/about_people/peeps.js
+++ frontend/components/about_people/peeps.js
@@ -1,33 +1,33 @@
 const peeps = [{
 	background: '#2980B9',
-	imgSrc: 'https://i.imgur.com/OG1l4x5.jpg',
+	imgSrc: './assets/tc_headshotphoto_2.jpg',
 	imgBorderColor: '#015389',
-	name: 'Tom Chesnutt',
-	title: 'Ego Stroke',
-	mobileNo: '7608098868',
-  faxNo: '7609426853',
+	name: 'Thomas W. Chesnutt, Ph.D., PStat®, CAP®',
+	title: 'CEO',
+	mobileNo: '7609425149',
+    faxNo: '7609426853',
 	location: 'Encinitas, CA',
-	role: 'Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby.'
+	role: 'Tom Chesnutt has pioneered innovative water rate reform, probability management, stochastic simulation and forecasting in the fields of water policy and economic modeling.'
 }, {
 	background: '#27AE60',
 	imgSrc: './assets/dp_headshotphoto_1.jpg',
 	imgBorderColor: '#086C32',
 	name: 'David Pekelney',
-	title: 'Ego Stroke',
+	title: 'Director of Policy Analysis',
 	mobileNo: '3104391883',
-  faxNo: '3104391884',
+    faxNo: '3104391884',
 	location: 'Los Angeles, CA',
-	role: 'Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby.'
+	role: 'David Pekelney has extensive experience analyzing financial, and economic, environmental policies in the areas of urban water conservation and planning, water recycling, and water demand using quantitative and qualitative methods.'
 }, {
 	background: '#9B27AE',
-	imgSrc: 'https://i.imgur.com/OG1l4x5.jpg',
+	imgSrc: './assets/dh_headshotphoto_1.jpg',
 	imgBorderColor: '#6A067A',
 	name: 'Dana Holt',
-	title: 'Ego Stroke',
+	title: 'Information Scientist',
 	mobileNo: '7609425149',
-  faxNo: '7609426853',
+    faxNo: '7609426853',
 	location: 'Encinitas, CA',
-	role: 'Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby. Super short burlby.'
+	role: 'Dana Holt has over 26 years of experience in software and hardware project lifecycle development, software implementation, project management, technical writing, and training.'
 }];
 
 export default peeps;
