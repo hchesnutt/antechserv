0d6cea5e7ed719752f30112177180eb83a364ccd
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-04-18 11:54:53 -0700

    removed particles from splash added background images

--- frontend/components/splash/splash_with_particles_DEPRECIATED.jsx
+++ frontend/components/splash/splash_with_particles_DEPRECIATED.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import Params from './params.json';
 
 class Splash extends React.Component {
   constructor(props){
@@ -7,10 +9,14 @@ class Splash extends React.Component {
   }
 
   render(){
+    console.log(Params)
     return (
         <div className='splash'>
           <div className='splash-logo-area'>
             <img src={'./assets/an_logo_medium.png'} className='splash-logo'/>
+          </div>
+          <div className='splash-particles'>
+            <Particles params={Params}/>
           </div>
         </div>
     );
