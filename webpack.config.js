73afa8a2c96ba5acfc571a8679cc75d94f7b43cf
Henry <t.henry.chesnutt@gmail.com>
2018-12-04 21:12:50 -0800

    configure webpack for css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,12 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      }
     ],
   },
   devtool: "source-map",
