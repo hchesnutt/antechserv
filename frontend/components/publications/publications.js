4143f4fa651ecda054c44946e12e979a9789cdc6
Henry <t.henry.chesnutt@gmail.com>
2018-12-04 20:53:56 -0800

    update image paths to new bundle location

--- frontend/components/publications/publications.js
+++ frontend/components/publications/publications.js
@@ -1,16 +1,16 @@
 const Publications = [{
 	link_url: 'https://www.usbr.gov/lc/socal/reports/MesaWDFinalReport.pdf',
-	image: './assets/publication/AnalysisofWaterUseEfficiency.png',
+	image: '../assets/publication/AnalysisofWaterUseEfficiency.png',
 	header: 'Mesa Water District Water Use Efficiency Benchmarking Study and Plan Final Project Report (January 2014)',
 	text: 'The Mesa Water District serves over 110,000 customers throughout Costa Mesa, Newport Beach and unincorporated parts of Orange County. The Water Use Efficiency (WUE) Plan recommends a portfolio of WUE programs that can feasibly be implemented with a water cost of $198 per acre-foot. The Benchmark Analysis proposes up to three alternative measures to help benchmark performance in the future',
 },{
 	link_url: 'http://www.financingsustainablewater.org/tools/building-better-water-rates-uncertain-world',
-	image: './assets/publication/BuildingBetterWaterRatesHandbookCoverPicSmall.png',
+	image: '../assets/publication/BuildingBetterWaterRatesHandbookCoverPicSmall.png',
 	header: 'Orange County Water Use Efficiency Master Plan (June 2013)',
 	text: 'This plan was developed by the Municipal Water District of Orange County in coordination with its member agencies to help reach mandated water use reductions by 2020. It creates a strategy to meet specific reduction targets and offers customized tools to track performance and adapt to changing circumstances.',
 },{
 	link_url: 'https://www.usbr.gov/lc/socal/reports/MWDOCMasterPlan2013.pdf',
-	image: './assets/publication/MWDOC_WUE_MasterPlan2013.png',
+	image: '../assets/publication/MWDOC_WUE_MasterPlan2013.png',
 	header: 'Building Better Water Rates',
 	text: 'Water ratemaking is a complex analytical endeavor composed of many building blocks, each of which needs to be addressed before a utility can model and select a successful rate structure. Such critical steps as demand forecasting, carrying out a cost of service analysis, determining revenue requirements, and identifying objectives cannot be undervalued. Only after completing them can rate setters undertake the exercise — or art — of designing a rate structure and evaluating it against multiple scenarios to determine its effectiveness.',
 }];
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,7 +9,7 @@ class Splash extends React.Component {
     return (
         <div className='splash'>
           <div className='splash-logo-area'>
-            <img src={'./assets/an_logo/A&N_logotype_med.png'} className='splash-logo'/>
+            <img src={'../assets/an_logo/A&N_logotype_med.png'} className='splash-logo'/>
           </div>
         </div>
     );
