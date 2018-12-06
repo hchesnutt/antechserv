b607b47abc02d8c1404754ebd5a6940195d8824b
Henry <t.henry.chesnutt@gmail.com>
2018-12-05 19:55:39 -0800

    refactor pubs component to css modules

--- frontend/components/publications/pubs.jsx
+++ frontend/components/publications/pubs.jsx
@@ -3,16 +3,17 @@ import PubCard from './pub_card';
 import Publications from './publications';
 import { Element } from 'react-scroll';
 
+import styles from './pubs.css';
+
 class Pubs extends React.Component {
   constructor(props){
     super(props);
   }
 
-
   render(){
     return(
-      <Element name='pubs' className='pubs'>
-        <ul className='pubs-list'>
+      <Element name='pubs' className={styles.pubs}>
+        <ul className={styles.pubsList}>
           {Publications.map((pubSingle, i) => (
             <li key={i}>
               <PubCard {...pubSingle}/>
