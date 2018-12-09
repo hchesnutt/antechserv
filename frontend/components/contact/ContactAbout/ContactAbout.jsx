dd82657c15721019ebb1ab4b793631063919a7ea
Henry <t.henry.chesnutt@gmail.com>
2018-12-08 19:06:23 -0800

    add wireframe and boilerplate for Contact page

---
+++ frontend/components/contact/ContactAbout/ContactAbout.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import styles from './ContactAbout.css';
+
+class ContactAbout extends Component {
+  render() { 
+    return (
+      <div className={styles.contactAboutContainer}>
+        hello from ContactAbout
+      </div>
+    );
+  }
+}
+ 
+export default ContactAbout;
