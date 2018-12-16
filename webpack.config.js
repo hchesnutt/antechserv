876a9aabb1b0a368016e02b40fb6dbcc6bb2329e
Henry <t.henry.chesnutt@gmail.com>
2018-12-16 13:11:53 -0800

    merge changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./frontend/index.jsx",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     loaders: [
